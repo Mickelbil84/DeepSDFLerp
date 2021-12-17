e8ef42550f1dbe9823a0d843fca2a520a93eaf97
Michael Bilevich <mickelbil84@gmail.com>
2021-12-17 20:42:18 +0200

    Code cleanup, proper ML structure

--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 import tqdm
 import torch
+import visdom
 import numpy as np
-import pandas as pd
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader
@@ -9,30 +9,66 @@ from torch.utils.data import DataLoader
 import data
 import model
 
+NUM_EPOCHS = 10
+
+# Init Visdom
+vis = None 
 if __name__ == "__main__":
-    # Load chain data
+    vis = visdom.Visdom(env='deepsdf')
+
+
+def train(train_loader, model, criterion, optimizer, epoch, train_losses):
+    total_loss = 0
+    cnt = 0
+    train_enum = tqdm.tqdm(enumerate(train_loader), desc='Train epoch %d' % epoch)
+
+    for i, data in train_enum:
+        # Get xyz,latent as input and sdf and label
+        xyz = data['xyz'].cuda()
+        latent = data['latent'].cuda()
+        sdf = data['sdf'].cuda()
+
+        # Perform a learning step
+        model.zero_grad()
+        output = model(xyz, latent)
+        loss = criterion(output, sdf)
+        loss.backward()
+        optimizer.step()
+
+        # Update losses and visualize
+        total_loss += loss.item()
+        cnt += 1
+        if i % 1000 == 0:
+            train_enum.set_description('Train (loss %.4f) epoch %d' % (loss.item(), epoch))
+            train_losses.append(loss.item())
+            vis.line(Y=np.asarray(train_losses), X=torch.arange(1, 1+len(train_losses)),
+                opts={'title': 'Loss'}, name='loss', win='loss')
+    
+    # Print summary of epoch
+    print('=====> Train set loss: {:.4f}\t Epoch: {}'.format(total_loss / cnt, epoch))
+
+
+def main():
+    ###################
+    # Load train data
+    ###################
     train_dataset = data.DeepSDFDataset()
     train_loader = DataLoader(train_dataset, batch_size=128, num_workers=4, pin_memory=True, shuffle=True)
 
+    ###############################
+    # Prepare model and optimizers
+    ###############################
     deepsdf = model.DeepSDF().cuda()
-
     criterion = nn.MSELoss()
     optimizer = optim.Adam(deepsdf.parameters())
 
-    for epoch in range(1):
-        for i, data in tqdm.tqdm(enumerate(train_loader)):
-            xyz = data['xyz'].cuda()
-            latent = data['latent'].cuda()
-            sdf = data['sdf'].cuda()
+    ###############################
+    # Start training
+    ###############################
+    train_losses = []
+    for epoch in range(NUM_EPOCHS):
+        train(train_loader, deepsdf, criterion, optimizer, epoch, train_losses)
+        torch.save(deepsdf.state_dict(), 'checkpoints/checkpoint_e%d.pth' % epoch)
 
-            optimizer.zero_grad()
-
-            output = deepsdf(xyz, latent)
-            loss = criterion(output, sdf)
-            loss.backward()
-            optimizer.step()
-
-            if i % 1000 == 0:
-                print(loss.cpu().item())
-
-    torch.save(deepsdf.state_dict(), 'test2.pth')
+if __name__ == "__main__":
+    main()
